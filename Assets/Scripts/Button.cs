4872603d72a675a9176ba4955b4469f3bc8cdb14
carolynqu <67798872+carolynqu@users.noreply.github.com>
2023-02-06 14:57:41 +0100

    ball stop

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -5,32 +5,46 @@ using UnityEngine;
 public class Button : MonoBehaviour
 {
 
-    SpriteRenderer mySpriteRenderer;
+    SpriteRenderer sr;
     Rigidbody2D rb2d;
 
     public float speed = 5;
+
+    private Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        Move();
 
-        
-
-
-        //consistient speed in x direction
-        Vector3 movement = new Vector3();
-        movement.x = speed;
-        movement.y = 0;
-
-        rb2d.velocity = new Vector2(movement.x, movement.y);
-
+        if(Input.GetKey(KeyCode.Space))
+        {
+            StopButton();
+        }
 
     }
+
+    public void Move()
+    {
+        //consistient speed in x direction
+        movement.x = speed;
+
+        rb2d.velocity = new Vector2(movement.x, movement.y);
+    }
+
+    public void StopButton()
+    {
+        movement.x = 0;
+
+        rb2d.velocity = new Vector2(movement.x, movement.y);
+    }
+
+
 }
