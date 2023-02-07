06fbc6cbc3fc7321a5e2a55f70ec97763c7ab39a
carolynqu <67798872+carolynqu@users.noreply.github.com>
2023-02-07 15:00:11 +0100

    final

--- Assets/Scripts/quit.cs
+++ Assets/Scripts/quit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class quit : MonoBehaviour
+public class Quit : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
